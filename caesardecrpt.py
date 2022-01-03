efde70bef040b8ebf114e3cf2c1f2bb41f96fe3c
Srishty Mangutte <68679980+Srish-ty@users.noreply.github.com>
2022-01-03 20:14:36 +0530

    Add files via upload

---
+++ caesardecrpt.py
@@ -0,0 +1,26 @@
+keyy= int(input())
+
+inp= input().lower()
+
+alph="abcdefghijklmnopqrstuvwxyz"
+
+msg=""
+
+for lett in inp:
+    if lett in alph:
+        req= alph.index(lett)-keyy 
+#        if req > 25 :
+#           req=req-26
+#        else:
+#            req=req
+      
+        msg=msg+alph[req]
+  
+  
+    else:
+        msg=msg+lett
+
+print("key:"+str(keyy))
+print("your message:"+inp+"\n")
+
+print(" "+msg)
